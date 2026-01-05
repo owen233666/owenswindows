1c3458cd66c447694f8dd7753c715d4802e77135
owen233666 <2858084927@qq.com>
2026-01-05 22:23:45 +0800

    添加了数据生成

---
+++ src/main/java/com/owen233666/owenswindows/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,58 @@
+package com.owen233666.owenswindows.datagen;
+
+import com.owen233666.owenswindows.block.ModBlocks;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.OAK_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_OAK_WINDOW.get());
+        dropSelf(ModBlocks.OAK_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.SPRUCE_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_SPRUCE_WINDOW.get());
+        dropSelf(ModBlocks.SPRUCE_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.BIRCH_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_BIRCH_WINDOW.get());
+        dropSelf(ModBlocks.BIRCH_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.JUNGLE_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_JUNGLE_WINDOW.get());
+        dropSelf(ModBlocks.JUNGLE_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.ACACIA_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_ACACIA_WINDOW.get());
+        dropSelf(ModBlocks.ACACIA_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.DARK_OAK_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_DARK_OAK_WINDOW.get());
+        dropSelf(ModBlocks.DARK_OAK_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.MANGROVE_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_MANGROVE_WINDOW.get());
+        dropSelf(ModBlocks.MANGROVE_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.CHERRY_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_CHERRY_WINDOW.get());
+        dropSelf(ModBlocks.CHERRY_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.BAMBOO_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_BAMBOO_WINDOW.get());
+        dropSelf(ModBlocks.BAMBOO_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.CRIMSON_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_CRIMSON_WINDOW.get());
+        dropSelf(ModBlocks.CRIMSON_PLANK_WINDOW.get());
+        dropSelf(ModBlocks.WARPED_WINDOW.get());
+        dropSelf(ModBlocks.STRIPPED_WARPED_WINDOW.get());
+        dropSelf(ModBlocks.WARPED_PLANK_WINDOW.get());
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+}
